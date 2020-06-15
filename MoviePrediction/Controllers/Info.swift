3350c1d058c749dd3725047e54f668158e8e46d1
Yogesh <yogeshpatel984.yp@gmail.com>
2020-06-15 14:21:20 +0530

    my new project is ready

--- MoviePrediction/Controllers/Info.swift
+++ MoviePrediction/Controllers/Info.swift
@@ -10,17 +10,30 @@ import UIKit
 
 class Info: ViewController,UITableViewDataSource,UITableViewDelegate {
     
-    var arr = [["Image":#imageLiteral(resourceName: "220px-Shubh_Mangal_Zyada_Saavdhan_poster"),"Name":"Shubh Mangal Zyada Savdhaan","Description":"Ayushman Khurrana,Jitendra Kumar,Gujraj Rao,Neena Gupta"],
-           ["Image":#imageLiteral(resourceName: "IMG_6251"),"Name":"The Story of Gunjan Saxena","Description":"Janhvi Kapur,Pankaj Tripathi,Angad Bedi,veenit Kumar Singh,Manav Vij"],
-           ["Image":#imageLiteral(resourceName: "220px-Angrezi_Medium_film_poster"),"Name":"Angrezi Medium","Description":"Irrfan Khan,Radhika Madan,Karina Kapoor"],
-           ["Image":#imageLiteral(resourceName: "rooh-afza-et00099813-29-03-2019-01-55-30"),"Name":"Rooh Afza","Description":"Janhvi Kapoor,Rajkummar Rao,Varun Sharma"],
-           ["Image":#imageLiteral(resourceName: "images"),"Name":"’83","Description":"Ranveer singh,Deepika Padukone,Tahir Raj Bhasin,Saqib Saleem,Jiva,Harrdy Sandhu"],
-           ["Image":#imageLiteral(resourceName: "220px-Coolie_No._1_poster"),"Name":"Coolie No. 1","Description":"Varun Dhavan,Sara Ali Khan"],
-           ["Image":#imageLiteral(resourceName: "MV5BOWMwMTViMmQtNjUzMy00MWQzLWIxN2UtZDAwN2Q5OGQ2NWIyXkEyXkFqcGdeQXVyNjQxNDg4OTM@._V1_"),"Name":"Khaali Peeli","Description":"Ishaan Khatter, Ananya Panday"],
-           ["Image":#imageLiteral(resourceName: "sadak-2-et00072278-10-03-2018-03-43-47"),"Name":"Sadak 2","Description":"Alia Bhatt, Aditya Roy Kapur, Sanjay Dutt and Pooja Bhatt"]
-       
-       
-       ]
+//    var arr = [["Image":#imageLiteral(resourceName: "220px-Shubh_Mangal_Zyada_Saavdhan_poster"),"Name":"Shubh Mangal Zyada Savdhaan","Description":"Ayushman Khurrana,Jitendra Kumar,Gujraj Rao,Neena Gupta"],
+//           ["Image":#imageLiteral(resourceName: "IMG_6251"),"Name":"The Story of Gunjan Saxena","Description":"Janhvi Kapur,Pankaj Tripathi,Angad Bedi,veenit Kumar Singh,Manav Vij"],
+//           ["Image":#imageLiteral(resourceName: "220px-Angrezi_Medium_film_poster"),"Name":"Angrezi Medium","Description":"Irrfan Khan,Radhika Madan,Karina Kapoor"],
+//           ["Image":#imageLiteral(resourceName: "rooh-afza-et00099813-29-03-2019-01-55-30"),"Name":"Rooh Afza","Description":"Janhvi Kapoor,Rajkummar Rao,Varun Sharma"],
+//           ["Image":#imageLiteral(resourceName: "images"),"Name":"’83","Description":"Ranveer singh,Deepika Padukone,Tahir Raj Bhasin,Saqib Saleem,Jiva,Harrdy Sandhu"],
+//           ["Image":#imageLiteral(resourceName: "220px-Coolie_No._1_poster"),"Name":"Coolie No. 1","Description":"Varun Dhavan,Sara Ali Khan"],
+//           ["Image":#imageLiteral(resourceName: "MV5BOWMwMTViMmQtNjUzMy00MWQzLWIxN2UtZDAwN2Q5OGQ2NWIyXkEyXkFqcGdeQXVyNjQxNDg4OTM@._V1_"),"Name":"Khaali Peeli","Description":"Ishaan Khatter, Ananya Panday"],
+//           ["Image":#imageLiteral(resourceName: "sadak-2-et00072278-10-03-2018-03-43-47"),"Name":"Sadak 2","Description":"Alia Bhatt, Aditya Roy Kapur, Sanjay Dutt and Pooja Bhatt"]
+//
+//
+//       ]
+//
+    var aray = [["Image":#imageLiteral(resourceName: "220px-Shubh_Mangal_Zyada_Saavdhan_poster"),"Name":"Shubh Mangal Zyada Savdhaan","Description":"Ayushman Khurrana,Jitendra Kumar,Gujraj Rao,Neena Gupta"],
+                ["Image":#imageLiteral(resourceName: "Janhvi Kapoor- Gunjan Saxena The Kargil Girl"),"Name":"The Story of Gunjan Saxena","Description":"Janhvi Kapur,Pankaj Tripathi,Angad Bedi,veenit Kumar Singh,Manav Vij"],
+                ["Image":#imageLiteral(resourceName: "220px-Angrezi_Medium_film_poster"),"Name":"Angrezi Medium","Description":"Irrfan Khan,Radhika Madan,Karina Kapoor"],
+                 ["Image":#imageLiteral(resourceName: "rooh-afza-et00099813-29-03-2019-01-55-30"),"Name":"Rooh Afza","Description":"Janhvi Kapoor,Rajkummar Rao,Varun Sharma"],
+                 ["Image":#imageLiteral(resourceName: "images"),"Name":"’83","Description":"Ranveer singh,Deepika Padukone,Tahir Raj Bhasin,Saqib Saleem,Jiva,Harrdy Sandhu"],
+                  ["Image":#imageLiteral(resourceName: "220px-Coolie_No._1_poster"),"Name":"Coolie No. 1","Description":"Varun Dhavan,Sara Ali Khan"],
+                   ["Image":#imageLiteral(resourceName: "MV5BOWMwMTViMmQtNjUzMy00MWQzLWIxN2UtZDAwN2Q5OGQ2NWIyXkEyXkFqcGdeQXVyNjQxNDg4OTM@._V1_"),"Name":"Khaali Peeli","Description":"Ishaan Khatter, Ananya Panday"],
+                   ["Image":#imageLiteral(resourceName: "sadak-2-et00072278-10-03-2018-03-43-47"),"Name":"Sadak 2","Description":"Alia Bhatt, Aditya Roy Kapur, Sanjay Dutt and Pooja Bhatt"],
+                   
+                
+    ]
+    
       
 
     override func viewDidLoad() {
@@ -34,15 +47,15 @@ class Info: ViewController,UITableViewDataSource,UITableViewDelegate {
           }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
+        return aray.count
       }
       
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as!  TableViewCell
                  
-        cell.img.image = self.arr[indexPath.row]["Image"] as? UIImage
-        cell.name.text = self.arr[indexPath.row]["Name"] as? String
-        cell.detail.text = self.arr[indexPath.row]["Description"] as? String
+        cell.img.image = self.aray[indexPath.row]["Image"] as? UIImage
+        cell.name.text = self.aray[indexPath.row]["Name"] as? String
+        cell.detail.text = self.aray[indexPath.row]["Description"] as? String
                  
                  cell.layer.cornerRadius = 10.0
                  cell.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
